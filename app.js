12cea4bac2814630385ec53514d75972e091cdd8
degrassed-mastermind <degrassed@gmail.com>
2026-06-21 20:34:42 -0400

    Use honeycomb image for app header background

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const SUPABASE_CONFIG = {
 };
 
 const PLATFORM_REVIEWER_EMAIL = "degrassed@gmail.com";
-const APP_VERSION = "v178";
+const APP_VERSION = "v179";
 
 const PERIOD_FORMATS = {
   quarters: {
