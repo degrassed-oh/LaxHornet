05eefe69705bfd01cf89cac73e7fab054fe46615
degrassed-mastermind <degrassed@gmail.com>
2026-06-27 07:28:14 -0400

    Bump app release to v240

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const SUPABASE_CONFIG = {
 };
 
 const PLATFORM_REVIEWER_EMAIL = "degrassed@gmail.com";
-const APP_VERSION = "v239";
+const APP_VERSION = "v240";
 
 const PERIOD_FORMATS = {
   quarters: {
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "laxhornet-v239";
+const CACHE_NAME = "laxhornet-v240";
 const APP_ASSETS = [
   "./",
   "./index.html",
