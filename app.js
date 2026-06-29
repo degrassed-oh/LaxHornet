f2d0977dd1c9a5337534acc3b8597a6bc3a85876
degrassed-mastermind <degrassed@gmail.com>
2026-06-28 21:56:26 -0400

    Bump app release to v255

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const SUPABASE_CONFIG = {
 };
 
 const PLATFORM_REVIEWER_EMAIL = "degrassed@gmail.com";
-const APP_VERSION = "v254";
+const APP_VERSION = "v255";
 
 const PERIOD_FORMATS = {
   quarters: {
